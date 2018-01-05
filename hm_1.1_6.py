2337537cd49d828003ab485bf0c57837287edf9a
Vance23 <34525041+Vance23@users.noreply.github.com>
2018-01-05 23:18:36 +0200

    Update hm_1.1_6.py
    откорректировал

--- hm_1.1_6.py
+++ hm_1.1_6.py
@@ -13,17 +13,17 @@ x2 = ((a**2 + b**2) % 2)
 print(x2)
 
 # Ex.3
-x3 = ( a + b ) / 12 * c % 4 + b
+x3 = (a + b) / 12 * c % 4 + b
 print(x3)
 
 # Ex.4
-x4 = (a - b * c ) / ( a + b ) % c
+x4 = (a - b * c) / (a + b) % c
 print(x4)
 
 # Ex.5
-x5 =  abs (a - b) /( a + b)**3 - math.cos( c )
+x5 =  abs(a - b) / (a + b)**3 - math.cos(c)
 print(x5)
 
 # Ex.6
-x6 = ( math.log ( 1 + c ) / - b )**4 + abs (a)
+x6 = (math.log(1 + c) / - b)**4 + abs(a)
 print(x6)

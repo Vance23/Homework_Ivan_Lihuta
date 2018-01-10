8fb074001f543ff647e74d79ed180d7870e7a421
Vance23 <34525041+Vance23@users.noreply.github.com>
2018-01-11 00:37:31 +0200

    Update hw_14.py

--- hw_14.py
+++ hw_14.py
@@ -9,12 +9,12 @@ print (is_even(11))
 print (is_even(10))
 print (' ')
 
-def is_even(number):
+def is_uneven(number):
     '''проверка не четности некоторого числа'''
-    print (is_even.__doc__)
+    print (is_uneven.__doc__)
     if number % 2 != 0:
         return True
     else:
         return False
-print (is_even(11))
-print (is_even(10))
+print (is_uneven(11))
+print (is_uneven(10))

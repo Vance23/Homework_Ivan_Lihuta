f4d50d1bdec647624fe083dac8eb64e801b0a5f1
Vance23 <34525041+Vance23@users.noreply.github.com>
2018-01-11 22:25:31 +0200

    сделал одну строку.

--- hw_14.py
+++ hw_14.py
@@ -1,20 +1,13 @@
 def is_even(number):
     '''проверка четности некоторого числа'''
     print (is_even.__doc__)
-    if number % 2 == 0:
-        return True
-    else:
-        return False
-print (is_even(11))
+    return ('Even:', number % 2 == 0)
 print (is_even(10))
 print (' ')
 
 def is_uneven(number):
     '''проверка не четности некоторого числа'''
     print (is_uneven.__doc__)
-    if number % 2 != 0:
-        return True
-    else:
-        return False
+    return ('Unven:', number % 2 != 0)
 print (is_uneven(11))
-print (is_uneven(10))
+

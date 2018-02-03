a002528f6ce149c0287240e09bbfad1e4885160f
Vance23 <34525041+Vance23@users.noreply.github.com>
2018-02-03 21:47:03 +0200

    Update tests_10.py

--- tests_10.py
+++ tests_10.py
@@ -1,48 +1,66 @@
 import random
 
+
 while True:
 
-    Rock = 1
-    Scissors = 2
-    Paper = 3
+    ROCK = 1
+    SCISSORS = 2
+    PAPER = 3
+    QUIT = 4
 
-    user = int(input('You choice --> Rock(1), Scissors (2), Paper(3):\n'))
+    user = int(input('Choice: Rock(1), Scissors (2), Paper(3), Quit(4):\n'))
 
-    if user == 1:
+    if user == 4:
+        print ('Bye!')
+        break
+
+    if user == ROCK:
         print('Rock!')
 
-    elif user == 2:
+    elif user == SCISSORS:
         print('Scissors!')
 
-    elif user == 3:
+    elif user == PAPER:
         print('Paper!')
 
     else:
-        print('Wrong value! - Try again -->')
+        print('Wrong value! - Try again ->')
         continue
 
     comp = (random.randint(1, 3))
     print('Comp choice: %d' % (comp))
 
-    if comp == 1:
+    if comp == ROCK:
         print('Rock!')
 
-    elif comp == 2:
+    elif comp == SCISSORS:
         print('Scissors!')
 
-    elif comp == 3:
+    elif comp == PAPER:
         print('Paper!')
 
     if comp == user:
         print('Draw!')
 
-    elif comp == 2 and user == 1 or comp == 3 and user == 2 or comp == 1 and user == 3:
-        print('You wine!')
+    elif comp == SCISSORS and user == ROCK or comp == PAPER and user == SCISSORS or comp == ROCK and user == PAPER:
+        print('You won!')
+
+        total_user_victory = 0
+        while total_user_victory < 3:
+            total_user_victory = total_user_victory + 1
+        else:
+            print('Game Over!')
+        break
 
     else:
-        print('Comp wine!')
+        print('Comp won!')
+        total_comp_victory = 0
+        while total_comp_victory < 3:
+            total_comp_victory = total_comp_victory + 1
+        else:
+            print('Game Over!')
+        break
 
-# пока не правильно прописать ограничение цикла до 3-х побед
 
 
 
